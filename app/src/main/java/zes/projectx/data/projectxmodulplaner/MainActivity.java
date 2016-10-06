09e10f640145998314297a06c5014cdb989f77ae
devilbreaker <saif_samir@hotmail.de>
2016-10-06 20:30:44 +0200

    Initial commit

---
+++ app/src/main/java/zes/projectx/data/projectxmodulplaner/MainActivity.java
@@ -0,0 +1,13 @@
+package zes.projectx.data.projectxmodulplaner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
