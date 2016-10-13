5eb01605f867b196f789908b98bf509a70be84c0
devilbreaker <saif_samir@hotmail.de>
2016-10-13 15:44:02 +0200

    Saif: MainParser: readLogcat + readInternalStorage beendet Hinweis: Die Methoden sind noch nicht getestet.
    Methoden Test folgt, wenn die Login-Klasse besteht

--- app/src/main/java/zes/projectx/data/projectxmodulplaner/SourceFiles/MainParser.java
+++ app/src/main/java/zes/projectx/data/projectxmodulplaner/SourceFiles/MainParser.java
@@ -40,36 +40,12 @@ public class MainParser extends AsyncTask<Context, User, Void>{
 
     public void createLogCat(User x, Context con){
 
-        ArrayList<Subject> mysubs = UserManager.getInstance().get("MY");
-        ArrayList<Subject> all = UserManager.getInstance().get("ALL");
-
         try {
             // AssetsManager holt sich die Daten aus dem assetsordner (Speziell interne Funktion in Android)
             AssetManager am = con.getApplicationContext().getAssets();
             InputStream str = am.open("modul.csv"); //öffnet die zu parsende Datei
-            BufferedReader FileReader= new BufferedReader(new InputStreamReader(str));
-
-            String zeile="";
-            while(null!=(zeile=FileReader.readLine())){        //Solange die Zeile nicht null ist, lies die Daten
-            //IDEE: Leg die Daten erst lokal ab, danach füg sie in das Handy auf ROOT
-                String[] split=zeile.split(";");                //hier wird die Zeile zerlegt als Trennzeichen ;
-
-                //______GEPARSTE DATEN_______//
-                int id = new Integer(split[0]);
-                String name = split[1];
-                String kuerzel = split[2];
-                int cp = new Integer(split[3]);
-                String prof = split[4];
-                int aUe = new Integer (split[5]);
-                String site = split[6];
-                String uesite = split[7];
-                char bereich = split[8].charAt(0);
-                int sem = new Integer (split[9]);
-                double note = new Double (split[10]);
-                Subject sub = new Subject(id,name,kuerzel,cp,prof,aUe,site,uesite,bereich,sem,note);
-                mysubs.add(sub);
-                all.add(sub);
-            }
+            BufferedReader fileReader= new BufferedReader(new InputStreamReader(str));
+            readInternaltStorage(fileReader);
 
             saveLogCat(con);
         } catch (Exception e) {
@@ -77,7 +53,47 @@ public class MainParser extends AsyncTask<Context, User, Void>{
         }
     }
 
+    /**
+     * liest die einzelnen Daten aus dem BufferedReader heraus, der Zugriff auf den Storage hat
+     * @param file
+     * @throws IOException
+     */
+    private void readInternaltStorage(BufferedReader file) throws IOException {
 
+
+        ArrayList<Subject> mysubs = UserManager.getInstance().get("MY");
+        ArrayList<Subject> all = UserManager.getInstance().get("ALL");
+
+
+        String zeile="";
+        while(null!=(zeile=file.readLine())){        //Solange die Zeile nicht null ist, lies die Daten
+            //IDEE: Leg die Daten erst lokal ab, danach füg sie in das Handy auf ROOT
+            String[] split=zeile.split(";");                //hier wird die Zeile zerlegt als Trennzeichen ;
+
+            //______GEPARSTE DATEN_______//
+            int id = new Integer(split[0]);
+            String name = split[1];
+            String kuerzel = split[2];
+            int cp = new Integer(split[3]);
+            String prof = split[4];
+            int aUe = new Integer (split[5]);
+            String site = split[6];
+            String uesite = split[7];
+            char bereich = split[8].charAt(0);
+            int sem = new Integer (split[9]);
+            double note = new Double (split[10]);
+            Subject sub = new Subject(id,name,kuerzel,cp,prof,aUe,site,uesite,bereich,sem,note);
+            mysubs.add(sub);
+            all.add(sub);
+        }
+
+    }
+
+
+    /**
+     * speichert die aktuellen Daten ins Logcat
+     * @param context
+     */
     public void saveLogCat(Context context) {
         FileOutputStream outputStream = null;
         ArrayList<Subject> all = UserManager.getInstance().get("ALL");
@@ -114,9 +130,36 @@ public class MainParser extends AsyncTask<Context, User, Void>{
     }
 
 
+    /**
+     * liest ein existierenden Logcat, falls es nichts findet, erzeugt es ein neues
+     * @param context
+     */
+    public void readLogcat(Context context){
+        FileInputStream inputStream = null;
+        User u = UserManager.getInstance().getUser();
+
+
+        try {
+            inputStream = context.openFileInput("logcat.txt");
+            BufferedReader fileReader= new BufferedReader(new InputStreamReader(inputStream));
+            readInternaltStorage(fileReader);
+
+
+        } catch (FileNotFoundException e) {
+            // Wenn "openFileInput" die datei nicht gefunden hat, wird er sie erzeugen, aber eine Exception auswerfen
+            createLogCat(u,context);
+
+        } catch (IOException e) {
+            //Wenn etwas mit dem FileReader selbst nicht stimmt, sollte diese Exception das Programm abbrechen
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected Void doInBackground(Context... params) {
+        Context context = params[0];
         dir = params[0].getFilesDir().getAbsolutePath();
+        readLogcat(context);
         return null;
     }
 }
