8ab65d04fdafd1ccaa493f29c23b93070c101dac
devilbreaker <saif_samir@hotmail.de>
2016-10-22 02:48:28 +0200

    Saif: AddSubject: Code wurde auf Zeeshans mail Code aktualisiert
    neue daten sind dazugefügt worden

--- app/src/main/java/zes/projectx/data/projectxmodulplaner/Activitys/AddSubject.java
+++ app/src/main/java/zes/projectx/data/projectxmodulplaner/Activitys/AddSubject.java
@@ -1,15 +1,19 @@
 package zes.projectx.data.projectxmodulplaner.Activitys;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -18,6 +22,7 @@ import zes.projectx.data.projectxmodulplaner.SourceFiles.Subject;
 import zes.projectx.data.projectxmodulplaner.SourceFiles.UserManager;
 
 public class AddSubject extends AppCompatActivity implements Runnable{
+    private ScrollView sc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +30,7 @@ public class AddSubject extends AppCompatActivity implements Runnable{
         setContentView(R.layout.activity_add_subject);
         Thread t = new Thread(this);
         t.run();
+        //sc = (ScrollView) findViewById(R.id.)
         try {
             t.join();
         } catch (InterruptedException e) {
@@ -34,75 +40,47 @@ public class AddSubject extends AppCompatActivity implements Runnable{
 
     @Override
     public void run() {
+        int i = 0;
 
+        ArrayList<Subject> allsub = UserManager.getInstance().get("ALL");
+        final int ROW_NUM = (allsub.size())/3;
+        final int COL_NUM = 3;
+        final TableLayout tableLayout = (TableLayout) findViewById(R.id.TableLayout);
+        for(int row = 0; row < ROW_NUM; row++){
 
-        //  DisplayMetrics dm = new DisplayMetrics();
-        //  getWindowManager().getDefaultDisplay().getMetrics(dm);
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f ));
 
-        //  int width = dm.widthPixels;
-        //  int height = dm.heightPixels;
+            tableLayout.addView(tableRow);
+            for(int col = 0; col < COL_NUM; col++){
+                final Button button = new Button(this);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        350,
+                        350,
+                        1.0f ));
+                button.setPadding(0,0,0,0);
+                button.setText(allsub.get(i).getKuerzel());
+                button.setTextSize(30);
+                button.setTypeface(Typeface.DEFAULT_BOLD);
+                i++;
 
-        //  getWindow().setLayout((int )(width*.9), (int) (height*.4));
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(button.getTypeface().getStyle() == Typeface.BOLD){
 
-        UserManager um = UserManager.getInstance();
-        ArrayList<Subject> all = um.get("MY");
-        int size = all.size();
-        Log.d("VariableALL", size + "");
+                            Toast.makeText(tableLayout.getContext(), button.getText() + " ausgewählt", Toast.LENGTH_SHORT).show();
+                            button.setTypeface(Typeface.SANS_SERIF);
+                        }
+                        else{button.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));}
+                    }
+                });
+                tableRow.addView(button);
 
-
-        /*
-    xmlns:tools="http://schemas.android.com/tools"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent"
-    android:paddingBottom="@dimen/activity_vertical_margin"
-    android:paddingLeft="@dimen/activity_horizontal_margin"
-    android:paddingRight="@dimen/activity_horizontal_margin"
-    android:paddingTop="@dimen/activity_vertical_margin"
-    tools:context="com.example.na.popupwindow.MainActivity">
-         */
-
-        //TODO Dynamisierung des Code oben drüber nicht fertig
-        TableLayout tableLayout = new TableLayout(this);
-        TableLayout.LayoutParams tlparam = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        tableLayout.setLayoutParams(tlparam);
-
-        /*
-    <Button
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:text="@string/ModulKürzel"
-        android:id="@+id/buttonDS"
-        android:layout_marginTop="25dp"
-        android:layout_alignParentTop="true"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
-        />
-        */
-
-
-        for(int i = 0; i < size; i++){
-           // TableRow tr = new TableRow(this);
-           // TableRow.LayoutParams trparam = new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            //tr.setOrientation(LinearLayout.HORIZONTAL);
-           // tr.setLayoutParams(trparam);
-           // for(int row = 0; row < 3; row ++){
-                //Wähle Sub aus:
-                Subject sub = all.get(i);
-                // Erzeuge den Button und seine Parameter als WrapContent
-                Button button = new Button(this);
-                ViewGroup.LayoutParams btnparam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-                button.setLayoutParams(btnparam);
-                //Text mit dem Kürzel festlegen:
-                button.setText(sub.getKuerzel());
-
-            //    tr.addView(button);
-            tableLayout.addView(button);
             }
-
-          //  tableLayout.addView(tr);
-       // }
-
-        setContentView(tableLayout);
-
+        }
     }
 }
