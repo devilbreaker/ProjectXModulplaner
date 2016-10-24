986e6c984d6242d560a8ad9072459d463b7519c3
Zishan Ahmad <zahmad94@gmail.com>
2016-10-24 18:34:10 +0200

    Merge branch 'master' of C:\Users\NA\AndroidStudioProjects\ProjectXModulplaner with conflicts.

--- app/src/main/java/zes/projectx/data/projectxmodulplaner/Activitys/AddSubject.java
+++ app/src/main/java/zes/projectx/data/projectxmodulplaner/Activitys/AddSubject.java
@@ -9,8 +9,8 @@ import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.Toast;
@@ -21,48 +21,116 @@ import zes.projectx.data.projectxmodulplaner.R;
 import zes.projectx.data.projectxmodulplaner.SourceFiles.Subject;
 import zes.projectx.data.projectxmodulplaner.SourceFiles.UserManager;
 
-public class AddSubject extends AppCompatActivity implements Runnable{
-    private ScrollView sc;
+public class AddSubject extends AppCompatActivity {
+
+    private static final int COL_NUM = 3;
+    private static int ROW_NUM = 0;
+    ImageButton checkButton;
+    ArrayList<Subject> all;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subject);
-        Thread t = new Thread(this);
-        t.run();
-        //sc = (ScrollView) findViewById(R.id.)
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        checkButton = (ImageButton) findViewById(R.id.CheckButton);
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AddSubject.this, "Noch keine Implementierung", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+      //  DisplayMetrics dm = new DisplayMetrics();
+      //  getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+      //  int width = dm.widthPixels;
+      //  int height = dm.heightPixels;
+
+      //  getWindow().setLayout((int )(width*.9), (int) (height*.4));
+
+        UserManager um = UserManager.getInstance();
+        all = um.get("ALL");
+        int size = all.size();
+        ROW_NUM = size/3;
+        Log.d("Variable", size + "");
+
+        addButton();
+
+
+        /*
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:paddingBottom="@dimen/activity_vertical_margin"
+    android:paddingLeft="@dimen/activity_horizontal_margin"
+    android:paddingRight="@dimen/activity_horizontal_margin"
+    android:paddingTop="@dimen/activity_vertical_margin"
+    tools:context="com.example.na.popupwindow.MainActivity">
+         */
+
+        //TODO Dynamisierung des Code oben drüber nicht fertig
+        TableLayout tableLayout = new TableLayout(this);
+        TableLayout.LayoutParams tlparam = new TableLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        tableLayout.setLayoutParams(tlparam);
+
+        /*
+    <Button
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:text="@string/ModulKürzel"
+        android:id="@+id/buttonDS"
+        android:layout_marginTop="25dp"
+        android:layout_alignParentTop="true"
+        android:layout_alignParentLeft="true"
+        android:layout_alignParentStart="true"
+        />
+        */
+
+
+        for(int i = 0; i < size; i++){
+            TableRow tr = new TableRow(this);
+            tr.setOrientation(LinearLayout.HORIZONTAL);
+            for(int row = 0; row < 3; row ++){
+                //Wähle Sub aus:
+                Subject sub = all.get(i);
+                // Erzeuge den Button und seine Parameter als WrapContent
+                Button button = new Button(this);
+                ViewGroup.LayoutParams btnparam = new LinearLayoutCompat.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                button.setLayoutParams(btnparam);
+                //Text mit dem Kürzel festlegen:
+                button.setText(sub.getKuerzel());
+                button.setPadding(0,25,0,0);
+                tr.addView(button);
+            }
+            tableLayout.addView(tr);
         }
+
+        setContentView(tableLayout);
     }
 
-    @Override
-    public void run() {
+    private void addButton() {
         int i = 0;
-
-        ArrayList<Subject> allsub = UserManager.getInstance().get("ALL");
-        final int ROW_NUM = (allsub.size())/3;
-        final int COL_NUM = 3;
-        final TableLayout tableLayout = (TableLayout) findViewById(R.id.TableLayout);
+        TableLayout tableLayout = (TableLayout) findViewById(R.id.TableLayout);
         for(int row = 0; row < ROW_NUM; row++){
 
-            TableRow tableRow = new TableRow(this);
+            TableRow tableRow = new TableRow(AddSubject.this);
             tableRow.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT,
                     TableLayout.LayoutParams.MATCH_PARENT,
-                    1.0f ));
-
+                    1.0f));
             tableLayout.addView(tableRow);
-            for(int col = 0; col < COL_NUM; col++){
-                final Button button = new Button(this);
+            for(int col = 0; col < COL_NUM; col++) {
+                Subject subject = all.get(i);
+                final Button button = new Button(AddSubject.this);
                 button.setLayoutParams(new TableRow.LayoutParams(
                         350,
                         350,
-                        1.0f ));
-                button.setPadding(0,0,0,0);
-                button.setText(allsub.get(i).getKuerzel());
+                        1.0f));
+                button.setPadding(0, 0, 0, 0);
+                button.setText(subject.getKuerzel());
                 button.setTextSize(30);
                 button.setTypeface(Typeface.DEFAULT_BOLD);
                 i++;
@@ -72,15 +140,16 @@ public class AddSubject extends AppCompatActivity implements Runnable{
                     public void onClick(View v) {
                         if(button.getTypeface().getStyle() == Typeface.BOLD){
 
-                            Toast.makeText(tableLayout.getContext(), button.getText() + " ausgewählt", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(AddSubject.this, button.getText() + " ausgewählt", Toast.LENGTH_SHORT).show();
                             button.setTypeface(Typeface.SANS_SERIF);
+
                         }
                         else{button.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));}
                     }
                 });
                 tableRow.addView(button);
-
             }
         }
+
     }
 }
